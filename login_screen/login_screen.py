d10722d7ad83f2b925b033cb79585860ae35b6d1
Aurora Pariseau <aurora.pariseau@wdc.com>
2021-06-18 13:29:46 -0500

    Developing login methods

--- login_screen/login_screen.py
+++ login_screen/login_screen.py
@@ -1,9 +1,16 @@
 from utils import load_kv
 
+from kivyauth.google_auth import initialize_google, login_google, logout_google
+from kivyauth.utils import login_providers
+
 from kivymd.uix.screen import Screen
 
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+
+# don't store this client side. Use a webproxy to facilitate auth
+GOOGLE_CLIENT_ID = 'REMOVED'
+GOOGLE_SECRET = 'REMOVED'
 
 
 class PasswordDialogue(MDDialog):
@@ -14,29 +21,38 @@ class PasswordDialogue(MDDialog):
 
 class LoginScreen(Screen):
 
-    def show_data(self):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
 
-        if len(self.ids.password_textfield.text) < 1:
-            return
+        initialize_google(self.after_login,
+                          self.error_listener,
+                          GOOGLE_CLIENT_ID,
+                          GOOGLE_SECRET
+                          )
 
+    def start_google_login(self):
+        login_google()
+        self.current_provider = login_providers.google
+
+    def after_login(self, name, email, photo_url):
         self.dialogue = MDDialog(
-            type = 'custom',
-            title = "Forgot Password",
-            buttons = [
+            type='custom',
+            title="Login Successful",
+            buttons=[
                 MDFlatButton(
-                    text="OK"
-                ),
-                MDFlatButton(
-                    text="Cancel",
+                    text="OK",
                     on_release=self.dismiss
                 )
             ]
         )
-        self.dialogue.text = self.ids.password_textfield.text
+        self.dialogue.text = f"You have logged in with your Google Account\n{email}"
         self.dialogue.open()
 
-    def dismiss(self):
-        self.dialogue.dismiss()
+    def error_listener(self):
+        pass
+
+    def dismiss(self, widget):
+        self.dialogue.dismiss(force=True)
 
 load_kv()
 
